6cc3c000639390834b8cf173d2c2037d42816e71
Unknown <sheazds@gmail.com>
2018-06-07 14:12:06 -0700

    Fixed tournament pairing code
    Pairs are loaded and assigned divs with single loop
    Begun styling brackets

--- debatesoft/tournaments/brackets.php
+++ debatesoft/tournaments/brackets.php
@@ -14,80 +14,38 @@
 				ON judge_pairing.judge_id = judge.judge_id";
 	
 	echo "<div id='brackets'>";
-		//For each round in tournament
-		$i=1;
-		$sqlrounds = $conn->query("SELECT DISTINCT(round_id) FROM pairing");
-		while($rounds = $sqlrounds->fetch_assoc())
-		{	
-			echo "<div id='round".$i."'>";
-			$sqlpairs = $conn->query("SELECT DISTINCT(pairing_id) FROM pairing");
-			while($pairs = $sqlpairs->fetch_assoc())
-			{
-				echo "<div id='round".$i."'>";
-				$prev = null;
-				$result = $conn->query($fullpairings);
-				while($row = $result->fetch_assoc())
-				{
-					if (($pairs['pairing_id'] == $row['pairing_id'])
-						&&($rounds['round_id'] == $row['round_id']))
-					{
-						$current = $row["pairing_id"];
-						if ($current != $prev)
-						{
-							echo "<td>" . $row["pairing_id"]. "</td>";
-							echo "<td>" . $row["round_id"]. "</td>";
-							echo "<td>" . $row["team1_name"]. "</td>";
-							echo "<td>" . $row["team2_name"]. "</td>";
-							echo "<td>" . $row["room_name"]. "</td>";
-							echo "<td>" . $row["judge_first_name"]. "</td>";
-							echo "<td>" . $row["judge_last_name"]. "</td>";
-						}
-						else
-						{
-							echo "<td>" . $row["judge_first_name"]. "</td>";
-							echo "<td>" . $row["judge_last_name"]. "</td>";
-						}
-						$prev = $current;
-					}
-				}
-				echo "<br />";
-										
-				echo "</div>";
-			}
-			echo "</div>";
-			$i++;
-		}
-	echo "</div>";
-?>
-
-<?php
-	$result = $conn->query($fullpairings);
-	if ($result->num_rows > 0) {
-		// output data of each row
-		echo "<table>";
-			echo "<tr>";
-			echo "<th>pairing_id</th>";
-			echo "<th>round_id</th>";
-			echo "<th>team1_name</th>";
-			echo "<th>team2_name</th>";
-			echo "<th>room_name</th>";
-			echo "<th>Judge</th>";
-		echo "</tr>";
-		
+		$prevround = null;
+		$prevpair = null;
+		$result = $conn->query($fullpairings);
 		while($row = $result->fetch_assoc())
 		{
-			//if( $row["round_id"] == 1)
+			$currentround = $row['round_id'];
+			$currentpair = $row['pairing_id'];
+			
+			if ($currentpair != $prevpair)
+				if ($prevpair != null)
+					echo "</div></div>";
+			if ($currentround != $prevround)
 			{
-				echo "<tr>";
-					echo "<td>" . $row["pairing_id"]. "</td>";
-					echo "<td>" . $row["round_id"]. "</td>";
-					echo "<td>" . $row["team1_name"]. "</td>";
-					echo "<td>" . $row["team2_name"]. "</td>";
-					echo "<td>" . $row["room_name"]. "</td>";
-					echo "<td>" . $row["judge_first_name"]. "</td>";
-					echo "<td>" . $row["judge_last_name"]. "</td>";
-				echo "</tr>";
+				if ($prevround != null)
+					echo "</div>";
+				echo "<div id='round".$currentround."' class='round'>";
 			}
+			if ($currentpair != $prevpair)
+			{
+				echo "<div class='pair_spacer'>";
+				echo "<div id='pair".$currentpair."' class='pair'>";
+				echo "<div class='teamname'>".$row['team1_name']."</div>";
+				echo "<div class='teamname'>".$row['team2_name']."</div>";
+				echo "<div class='room'>".$row['room_name']."</div>";
+				echo "<div class='judgename'>".$row['judge_first_name']." ".$row['judge_last_name']."</div>";
+			}
+			else
+			{
+				echo "<div class='judgename'>".$row['judge_first_name']." ".$row['judge_last_name']."</div>";
+			}
+			$prevround = $currentround;
+			$prevpair = $currentpair;
 		}
-	}
+	echo "</div>";
 ?>
