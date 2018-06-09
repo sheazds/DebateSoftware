b6fc8e21724eb6b0a6b5bb409d3ae7f6cf295b60
Shea <sheazds@gmail.com>
2018-06-09 15:53:58 -0700

    Updated tournament brackets to new database

--- debatesoft/tournaments/brackets.php
+++ debatesoft/tournaments/brackets.php
@@ -1,52 +1,60 @@
 <?php
 	require_once '../dbconfig.php';
-	$fullpairings = "SELECT pairing.pairing_id, pairing.round_id, team1.team_name AS team1_name, team2.team_name AS team2_name, room.room_name, judge.judge_first_name, judge.judge_last_name
-			FROM pairing
-			INNER JOIN team AS team1
-				ON pairing.team1_id = team1.team_id
-			INNER JOIN team AS team2
-				ON pairing.team2_id = team2.team_id
+	$pairingrooms = "SELECT pairing.pairing_id, pairing.round_id, room.room_name
+		FROM pairing
 			INNER JOIN room
-				ON pairing.room_id = room.room_id
-			INNER JOIN judge_pairing
-				ON pairing.pairing_id = judge_pairing.pairing_id
-			INNER JOIN judge
-				ON judge_pairing.judge_id = judge.judge_id";
+				ON pairing.room_id = room.room_id";
 	
 	echo "<div id='tournamentbrackets'>";
+	
 		$prevround = null;
-		$prevpair = null;
-		$result = $conn->query($fullpairings);
-		while($row = $result->fetch_assoc())
+		
+		$pairingresult = $conn->query($pairingrooms);
+		while($row = $pairingresult->fetch_assoc())
 		{
-			$currentround = $row['round_id'];
 			$currentpair = $row['pairing_id'];
+			$currentround = $row['round_id'];
 			
-			if ($currentpair != $prevpair)
-				if ($prevpair != null)
-					echo "</td></tr></tbody></table></div></div>";
-			if ($currentround != $prevround)
+			
+			if ($prevround != $currentround)
 			{
 				if ($prevround != null)
 					echo "</div>";
-				echo "<div id='round".$currentround."' class='round'>";
+				echo "<div id='round'".$currentround."' class='round'>";
 			}
-			if ($currentpair != $prevpair)
-			{
-				echo "<div class='pair_spacer'>";
-				echo "<div id='pair".$currentpair."' class='pair'>";
-				echo "<table><tbody>";
-				echo "<tr><td><b>Teams:</b></td><td>".$row['team1_name']."<br />".$row['team2_name']."</td></tr>";
-				echo "<tr><td><b>Room:</b></td><td>".$row['room_name']."</td></tr>";
-				echo "<tr><td><b>Judges:</b></td><td>".$row['judge_first_name']." ".$row['judge_last_name'];
-			}
-			else
-			{
-				echo "<br />".$row['judge_first_name']." ".$row['judge_last_name'];
-			}
+			
+			echo "<div class='pair_spacer'>";
+			echo "<div id='pair".$currentpair."' class='pair'>";
+			echo "<table><tbody>";
+			echo "<tr><td><b>Room:</b></td><td>".$row['room_name']."</td></tr>";
+			
+			echo "<tr><td><b>Teams:</b></td><td>";
+			$pairingteams = "SELECT pairing.pairing_id, team.team_name
+				FROM pairing
+					INNER JOIN pairing_team
+						ON pairing.pairing_id = pairing_team.pairing_id
+					INNER JOIN team
+						ON pairing_team.team_id = team.team_id
+					WHERE pairing.pairing_id = ".$currentpair;
+			$teamresult = $conn->query($pairingteams);
+			while($row = $teamresult->fetch_assoc())
+				echo $row['team_name']."<br />";
+			echo "</td></tr>";
+			
+			echo "<tr><td><b>Judges:</b></td><td>";
+			$judgepairings = "SELECT pairing.pairing_id, CONCAT(judge.judge_first_name,' ',judge.judge_last_name) AS judge_name
+				FROM pairing
+					INNER JOIN judge_pairing
+						ON pairing.pairing_id = judge_pairing.pairing_id
+					INNER JOIN judge
+						ON judge_pairing.judge_id = judge.judge_id
+					WHERE pairing.pairing_id = ".$currentpair;
+			$judgeresult = $conn->query($judgepairings);
+			while($row = $judgeresult->fetch_assoc())
+				echo $row['judge_name']."<br />";
+			echo "</td></tr></tbody></table></div></div>";
+			
 			$prevround = $currentround;
-			$prevpair = $currentpair;
 		}
-	echo "</td></tr></table></div></div></div>";
-	echo "</div>";
+	echo "</div></div>";
 ?>
